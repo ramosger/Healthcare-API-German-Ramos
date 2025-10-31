076a4f793b63b9e54e394452f833e779f59e7061
Germán Ramos <german.ramos@lightit.io>
2025-10-31 17:24:33 -0300

    feat: Get All Doctors

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Container\Attributes\CurrentUser;
 use Illuminate\Support\Facades\Route;
 use Lightit\Users\App\Controllers\{GetUserController, DeleteUserController, ListUserController, StoreUserController, UpdateUserController};
+use Lightit\Doctors\App\Controllers\{ListDoctorController};
 
 /*
 |--------------------------------------------------------------------------
@@ -41,4 +42,14 @@ Route::prefix('users')
             ->whereNumber('user');
         Route::delete('/{user}', DeleteUserController::class)
             ->whereNumber('user');
+    });
+
+/*
+|--------------------------------------------------------------------------
+| Doctors Routes
+|--------------------------------------------------------------------------
+*/
+Route::prefix('doctors')
+    ->group(static function (): void {
+        Route::get('/', ListDoctorController::class);
     });
