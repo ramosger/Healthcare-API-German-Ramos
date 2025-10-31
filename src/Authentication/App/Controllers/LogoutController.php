48da7ec255c67c1669a17e865c1cddbbb5375c01
Germán Ramos <german.ramos@lightit.io>
2025-10-31 10:47:27 -0300

    fix: Using Contextual attribute (Dependency Injection)

--- src/Authentication/App/Controllers/LogoutController.php
+++ src/Authentication/App/Controllers/LogoutController.php
@@ -4,25 +4,17 @@ declare(strict_types=1);
 
 namespace Lightit\Authentication\App\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Container\Attributes\CurrentUser;
 use Lightit\Authentication\Domain\Actions\LogoutAction;
-use Lightit\Models\JWTAuthenticatable;
-use Lightit\Shared\App\Exceptions\Http\UnauthorizedException;
+use Lightit\Users\Domain\Models\User;
+use Symfony\Component\HttpFoundation\Response;
 
 class LogoutController
 {
-    public function __invoke(Request $request, LogoutAction $logoutAction): Response
+    public function __invoke(#[CurrentUser] User $user, LogoutAction $logoutAction): Response
     {
-        /** @var JWTAuthenticatable|null $user */
-        $user = $request->user();
-
-        if (! $user) {
-            throw new UnauthorizedException();
-        }
-
         $logoutAction->execute();
 
-        return response()->noContent();
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
