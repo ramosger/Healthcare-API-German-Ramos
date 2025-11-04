69856624298fb945dc0ca0d40252246bc8885952
Germán Ramos <german.ramos@lightit.io>
2025-11-04 09:28:43 -0300

    feat: Delete Patient

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 use Lightit\Users\App\Controllers\{GetUserController, DeleteUserController, ListUserController, StoreUserController, UpdateUserController};
 use Lightit\Doctors\App\Controllers\{GetDoctorController, ListDoctorController, StoreDoctorController, UpdateDoctorController, DeleteDoctorController, AssignClinicsToDoctorController};
 use Lightit\Clinics\App\Controllers\{GetClinicController, ListClinicController, StoreClinicController, UpdateClinicController, DeleteClinicController, AssignDoctorsToClinicController};
-use Lightit\Patients\App\Controllers\{GetPatientController, ListPatientController, StorePatientController, UpdatePatientController};
+use Lightit\Patients\App\Controllers\{GetPatientController, ListPatientController, StorePatientController, UpdatePatientController, DeletePatientController};
 
 /*
 |--------------------------------------------------------------------------
@@ -93,6 +93,7 @@ Route::prefix('patients')->group(static function (): void {
     Route::prefix('{patient}')->group(static function (): void {
         Route::get('/', GetPatientController::class)->withTrashed();
         Route::put('/', UpdatePatientController::class);
+        Route::delete('/', DeletePatientController::class);
     })->whereNumber('patient');
 
     Route::get('/', ListPatientController::class);
---
+++ src/Patients/App/Controllers/DeletePatientController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Patients\App\Controllers;
+
+use Dedoc\Scramble\Attributes\Group;
+use Illuminate\Http\JsonResponse;
+use Lightit\Patients\Domain\Models\Patient;
+
+#[Group('Patients')]
+final readonly class DeletePatientController
+{
+    public function __invoke(Patient $patient): JsonResponse
+    {
+        $patient->delete();
+
+        return response()->json(null, JsonResponse::HTTP_NO_CONTENT);
+    }
+}
